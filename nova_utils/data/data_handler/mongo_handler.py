073441788e2bc16727ebd24d05b5ae531945a723
dominik <dominik.schiller@me.com>
2023-08-09 09:29:19 +0200

    Add performance prints Change and remove sensitive information

--- nova_utils/data/data_handler/mongo_handler.py
+++ nova_utils/data/data_handler/mongo_handler.py
@@ -1,8 +1,11 @@
-from nova_utils.data.data_handler.ihandler import IHandler
 import numpy as np
+import warnings
+from datetime import datetime
 from pymongo import MongoClient
+from pymongo.results import InsertOneResult, UpdateResult
 from typing import List
 from bson.objectid import ObjectId
+from nova_utils.data.data_handler.ihandler import IHandler
 from nova_utils.data.annotation import (
     LabelType,
     SchemeType,
@@ -53,85 +56,129 @@ class AnnotationMongoHandler(MongoHandler):
     Class for handling download of annotation data from Mongo db.
     """
 
-    def _find_one_by_name(self, name: str, collection: str, dataset: str):
-        query_filter = {"name": name}
-        result = self.client[dataset][collection].find_one(query_filter)
-        if result:
-            return result
-        else:
-            raise FileNotFoundError(
-                f'Could not find any entry for name "{name}" in collection {collection} in database {dataset}'
-            )
+    def _get_annotation(
+        self,
+        dataset: str,
+        session: str,
+        annotator: str,
+        role: str,
+        scheme: str,
+        project: dict = None,
+    ) -> dict:
+        pipeline = [
+            {
+                "$lookup": {
+                    "from": SESSION_COLLECTION,
+                    "localField": "session_id",
+                    "foreignField": "_id",
+                    "as": "session",
+                }
+            },
+            {
+                "$lookup": {
+                    "from": ANNOTATOR_COLLECTION,
+                    "localField": "annotator_id",
+                    "foreignField": "_id",
+                    "as": "annotator",
+                }
+            },
+            {
+                "$lookup": {
+                    "from": ROLE_COLLECTION,
+                    "localField": "role_id",
+                    "foreignField": "_id",
+                    "as": "role",
+                }
+            },
+            {
+                "$lookup": {
+                    "from": SCHEME_COLLECTION,
+                    "localField": "scheme_id",
+                    "foreignField": "_id",
+                    "as": "scheme",
+                }
+            },
+            {
+                "$match": {
+                    "$and": [
+                        {"role.name": role},
+                        {"session.name": session},
+                        {"annotator.name": annotator},
+                        {"scheme.name": scheme},
+                    ]
+                }
+            },
+            {
+                "$lookup": {
+                    "from": "AnnotationData",
+                    "localField": "data_id",
+                    "foreignField": "_id",
+                    "as": "data",
+                }
+            },
+        ]
 
-    def _get_session(self, dataset: str, session: str) -> dict:
-        return self._find_one_by_name(session, SESSION_COLLECTION, dataset)
+        # append projection
+        if project:
+            pipeline.append({"$project": project})
 
-    def _get_scheme(self, dataset: str, scheme: str) -> dict:
-        return self._find_one_by_name(scheme, SCHEME_COLLECTION, dataset)
+        result = list(self.client[dataset][ANNOTATION_COLLECTION].aggregate(pipeline))
+        if not result:
+            return {}
+        return result[0]
 
-    def _get_role(self, dataset: str, role: str) -> dict:
-        return self._find_one_by_name(role, ROLE_COLLECTION, dataset)
+    def _update_annotation(
+        self, dataset: str, anno_id: ObjectId, anno_data_id: ObjectId, anno: IAnnotation
+    ) -> tuple[UpdateResult, UpdateResult]:
 
-    def _get_annotator(self, dataset: str, annotator: str) -> dict:
-        return self._find_one_by_name(annotator, ANNOTATOR_COLLECTION, dataset)
+        # Todo consider mechanism to also update isLocked and isFinished attributes
+        update_query_annotation = {"$set": {"date": datetime.now()}}
+        update_query_annotation_data = {
+            "$set": {
+                "labels": [
+                    dict(zip(anno.annotation_scheme.label_dtype.names, ad.item()))
+                    for ad in anno.data
+                ]
+            }
+        }
+        result_anno = self.client[dataset][ANNOTATION_COLLECTION].update_one(
+            {"_id": anno_id}, update_query_annotation
+        )
+        result_anno_data = self.client[dataset][ANNOTATION_DATA_COLLECTION].update_one(
+            {"_id": anno_data_id}, update_query_annotation_data
+        )
 
-    def _get_anno_data(self, dataset: str, anno_id: ObjectId) -> dict:
-        query_filter = {"_id": anno_id}
-        result = self.client[dataset][ANNOTATION_DATA_COLLECTION].find_one(query_filter)
-        if result:
-            return result
-        else:
-            raise FileNotFoundError(
-                f'Could not find any entry for _id "{anno_id}" in collection {ANNOTATION_DATA_COLLECTION} in database {dataset}'
-            )
+        return (result_anno, result_anno_data)
+
+    def _insert_annotation_data(
+        self, dataset: str, annotation_data_doc: dict
+    ) -> InsertOneResult:
+        result = self.client[dataset][ANNOTATION_DATA_COLLECTION].insert_one(
+            annotation_data_doc
+        )
+        return result
 
     def load(
         self, dataset: str, scheme: str, session: str, annotator: str, role: str
     ) -> IAnnotation:
-        """
-        Load annotation data from Mongo db based on the provided parameters.
-
-        Parameters:
-            dataset (str): The dataset name.
-            scheme (str): The scheme name.
-            session (str): The session name.
-            annotator (str): The annotator name.
-            role (str): The roles for which annotations are requested.
-
-        Returns:
-            List[AnnotationData]: A list of annotation data objects.
-        """
 
         # load annotation from mongo db
-        session_object = self._get_session(dataset, session)
-        scheme_object = self._get_scheme(dataset, scheme)
-        annotator_object = self._get_annotator(dataset, annotator)
-        role_object = self._get_role(dataset, role)
+        anno_doc = self._get_annotation(dataset, session, annotator, role, scheme)
 
-        query_filter = {
-            "$and": [
-                {"session_id": session_object["_id"]},
-                {"scheme_id": scheme_object["_id"]},
-                {"annotator_id": annotator_object["_id"]},
-                {"role_id": role_object["_id"]},
-            ]
-        }
-        anno_doc = self.client[dataset][ANNOTATION_COLLECTION].find_one(query_filter)
         if not anno_doc:
             raise FileNotFoundError(
-                f'Could not find any entry for filter "{query_filter}" in collection {ANNOTATION_COLLECTION} in database {dataset}'
+                f"Can't overwrite locked annotation \ndataset: {dataset} \nsession: {session} \nannotator: {annotator} \nrole: {role} \nscheme: {scheme}"
             )
 
-        anno_data_doc = self._get_anno_data(dataset, anno_doc["data_id"])
+        (anno_data_doc,) = anno_doc["data"]
 
         # build annotation object
-        scheme_type = scheme_object["type"]
+        (scheme_doc,) = anno_doc["scheme"]
+        scheme_type = scheme_doc["type"]
 
         # discrete scheme
         if scheme_type == SchemeType.DISCRETE.name:
-            scheme_classes = {}
-            for item in scheme_object["labels"]:
-                scheme_classes[item.get("id")] = item.get("name")
+            scheme_classes = {l["id"]: l["name"] for l in scheme_doc["labels"]}
 
             anno_data = np.array(
                 [
@@ -152,9 +199,9 @@ class AnnotationMongoHandler(MongoHandler):
 
         # continuous scheme
         elif scheme_type == SchemeType.CONTINUOUS.name:
-            sr = scheme_object["sr"]
-            min_val = scheme_object["min"]
-            max_val = scheme_object["max"]
+            sr = scheme_doc["sr"]
+            min_val = scheme_doc["min"]
+            max_val = scheme_doc["max"]
             anno_data = np.array(
                 [(x["score"], x["conf"]) for x in anno_data_doc["labels"]],
                 dtype=LabelType.CONTINUOUS.value,
@@ -190,11 +237,21 @@ class AnnotationMongoHandler(MongoHandler):
                 data=anno_data,
             )
         else:
-            raise TypeError(f"Unknown scheme type {type}")
+            raise TypeError(f"Unknown scheme type {scheme_type}")
 
         return annotation
 
-    def save(self, annotation: str, session: str, dataset: str):
+    def save(
+        self,
+        annotation: IAnnotation,
+        dataset: str = None,
+        session: str = None,
+        annotator: str = None,
+        role: str = None,
+        is_finished: bool = False,
+        is_locked: bool = False,
+        overwrite: bool = False,
+    ):
         """
         Save annotation data to Mongo db.
 
@@ -204,9 +261,87 @@ class AnnotationMongoHandler(MongoHandler):
         Returns:
             None
         """
-        # Save annotation data to Mongo db
-        # Placeholder: Replace with the actual Mongo db save method based on your implementation
-        self.db_connection.save_annotation_data(annotation_data)
+        # overwrite default values
+        dataset = dataset if dataset else annotation.meta_info.dataset
+        session = session if session else annotation.meta_info.session
+        annotator = annotator if annotator else annotation.meta_info.annotator
+        role = role if role else annotation.meta_info.role
+        scheme = annotation.annotation_scheme.name
+
+        # TODO check for none values
+
+        # get documents
+        anno_doc = self._get_annotation(
+            dataset,
+            session,
+            annotator,
+            role,
+            scheme,
+            project={"_id": 1, "isLocked": 1, "data_id": 1},
+        )
+
+        # if anno already exists
+        if anno_doc:
+            if anno_doc["isLocked"]:
+                warnings.warn(
+                    f"Can't overwrite locked annotation \ndataset: {dataset} \nsession: {session} \nannotator: {annotator} \nrole: {role} \nscheme: {scheme}"
+                )
+                return None
+            elif not overwrite:
+                warnings.warn(
+                    f"Can't overwrite annotation \ndataset: {dataset} \nsession: {session} \nannotator: {annotator} \nrole: {role} \nscheme: {scheme}. Because overwrite is disabled."
+                )
+                return None
+            else:
+                warnings.warn(
+                    f"Overwriting existing annotation \ndataset: {dataset} \nsession: {session} \nannotator: {annotator} \nrole: {role} \nscheme: {scheme}"
+                )
+                success = self._update_annotation(
+                    dataset,
+                    anno_id=anno_doc["_id"],
+                    anno_data_id=anno_doc["data_id"],
+                    anno=annotation,
+                )
+                return success
+
+        # TODO: Do it!
+
+        # new anno
+        else:
+            success = self._insert_annotation_data(dataset, anno_data_doc)
+            if not success.acknowledged:
+                warnings.warn(
+                    f"Unexpected error uploading annotation \ndataset: {dataset} \nsession: {session} \nannotator: {annotator} \nrole: {role} \nscheme: {scheme}"
+                )
+                return None
+            else:
+                anno_data_doc_id = success.inserted_id
+
+            anno_doc = {
+                "data_id": anno_data_doc_id,
+                "annotator_id": annotator_doc[_id],
+                "role_id": mongo_role[0]["_id"],
+                "scheme_id": mongo_scheme[0]["_id"],
+                "session_id": mongo_session[0]["_id"],
+                "isFinished": is_finished,
+                "isLocked": is_locked,
+                "date": datetime.today().replace(microsecond=0),
+            }
+
+        success = self.update_doc_by_prop(
+            doc=mongo_label_doc,
+            database=database,
+            collection=self.ANNOTATION_DATA_COLLECTION,
+        )
+        if not success.acknowledged:
+            warnings.warn(
+                f"Unknown error update database entries for Annotation data {mongo_data_id}"
+            )
+            return ""
+        else:
+            data_id = mongo_data_id
+
+        # anno_data_doc = self._get_annotation_data(dataset, anno_doc["data_id"])
 
 
 class StreamDataHandler(MongoHandler):
@@ -247,13 +382,21 @@ class StreamDataHandler(MongoHandler):
 
 
 if __name__ == "__main__":
-    ip = "137.250.171.233"
-    port = 37317
-    user = "schildom"
-    password = "HsE8aFk8G.hGYzCe4dE"
+    import os
+    from time import perf_counter
+    from dotenv import load_dotenv
 
-    amh = AnnotationMongoHandler(ip=ip, port=port, user=user, password=password)
+    load_dotenv("../../../.env")
+    IP = os.getenv("NOVA_IP", "")
+    PORT = int(os.getenv("NOVA_PORT", 0))
+    USER = os.getenv("NOVA_USER", "")
+    PASSWORD = os.getenv("NOVA_PASSWORD", "")
 
+    amh = AnnotationMongoHandler(ip=IP, port=PORT, user=USER, password=PASSWORD)
+
+    # load
+    fs = "Loading {} took {}ms"
+    t_start = perf_counter()
     discrete_anno = amh.load(
         dataset="test",
         scheme="diarization",
@@ -261,6 +404,10 @@ if __name__ == "__main__":
         session="04_Oesterreich_test",
         role="testrole",
     )
+    t_stop = perf_counter()
+    print(fs.format("Discrete annotation", int((t_stop - t_start) * 1000)))
+
+    t_start = perf_counter()
     continuous_anno = amh.load(
         dataset="test",
         scheme="arousal",
@@ -268,6 +415,10 @@ if __name__ == "__main__":
         session="01_AffWild2_video1",
         role="testrole",
     )
+    t_stop = perf_counter()
+    print(fs.format("Continuous annotation", int((t_stop - t_start) * 1000)))
+
+    t_start = perf_counter()
     free_anno = amh.load(
         dataset="test",
         scheme="transcript",
@@ -275,3 +426,8 @@ if __name__ == "__main__":
         session="04_Oesterreich_test",
         role="testrole",
     )
+    t_stop = perf_counter()
+    print(fs.format("Free annotation", int((t_stop - t_start) * 1000)))
+
+    # save
+    amh.save(discrete_anno, overwrite=True)
